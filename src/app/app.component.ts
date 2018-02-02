f69e804a0a19e047b8a77f40212b3d902b1cb5f1
Joseph Chingalo <profschingalo@gmail.com>
2018-02-02 13:49:28 +0300

    Fixing reset to null for current user during log out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,11 +44,13 @@ export class MyApp {
 
   async logOut(){
     try{
-      let user :any = await this.userProvider.getCurrentUser();
-      user.isLogin = false;
-      this.userProvider.setCurrentUser(user).subscribe(()=>{
-        this.nav.setRoot('LoginPage');
+      this.userProvider.getCurrentUser().subscribe(user=>{
+        user.isLogin = false;
+        this.userProvider.setCurrentUser(user).subscribe(()=>{
+          this.nav.setRoot('LoginPage');
+        })
       })
+     
     }catch (e){
       console.log(JSON.stringify(e));
     }
