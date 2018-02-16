69e909f06c92e6a353315147b346a71a6d47c480
Joseph Chingalo <profschingalo@gmail.com>
2018-02-16 11:56:18 +0300

    Apply selection of language on setting module from support list dictionary

--- src/pages/setting/setting.ts
+++ src/pages/setting/setting.ts
@@ -1,10 +1,10 @@
-import { Component ,OnInit} from '@angular/core';
-import { IonicPage} from 'ionic-angular';
-import {SettingsProvider} from "../../providers/settings/settings";
-import {UserProvider} from "../../providers/user/user";
-import {AppProvider} from "../../providers/app/app";
-import {LocalInstanceProvider} from "../../providers/local-instance/local-instance";
-import {AppTranslationProvider} from "../../providers/app-translation/app-translation";
+import { Component, OnInit } from "@angular/core";
+import { IonicPage } from "ionic-angular";
+import { SettingsProvider } from "../../providers/settings/settings";
+import { UserProvider } from "../../providers/user/user";
+import { AppProvider } from "../../providers/app/app";
+import { LocalInstanceProvider } from "../../providers/local-instance/local-instance";
+import { AppTranslationProvider } from "../../providers/app-translation/app-translation";
 
 /**
  * Generated class for the SettingPage page.
@@ -15,85 +15,100 @@ import {AppTranslationProvider} from "../../providers/app-translation/app-transl
 
 @IonicPage()
 @Component({
-  selector: 'page-setting',
-  templateUrl: 'setting.html',
+  selector: "page-setting",
+  templateUrl: "setting.html"
 })
-export class SettingPage implements OnInit{
+export class SettingPage implements OnInit {
+  isSettingContentOpen: any;
+  settingContents: Array<any>;
 
-  isSettingContentOpen : any;
-  settingContents : Array<any>;
+  isLoading: boolean = false;
+  settingObject: any;
+  loadingMessage: string;
 
-  isLoading : boolean = false;
-  settingObject : any;
-  loadingMessage : string;
+  currentUser: any;
+  translationCodes: Array<any> = [];
+  currentLanguage: string;
+  localInstances: any;
 
-  currentUser : any;
-  currentLanguage : string;
-  localInstances : any;
+  constructor(
+    private settingsProvider: SettingsProvider,
+    private appProvider: AppProvider,
+    private localInstanceProvider: LocalInstanceProvider,
+    private appTranslationProvider: AppTranslationProvider,
+    private userProvider: UserProvider
+  ) {}
 
-
-  constructor(private settingsProvider : SettingsProvider,
-              private appProvider : AppProvider,
-              private localInstanceProvider : LocalInstanceProvider,
-              private appTranslationProvider : AppTranslationProvider,
-              private userProvider : UserProvider) {
-  }
-
-  ngOnInit(){
-    this.settingObject  = {};
-    this.loadingMessage = 'loading_current_user_information';
+  ngOnInit() {
+    this.settingObject = {};
+    this.translationCodes = this.appTranslationProvider.getSupportedTranslationObjects();
+    this.loadingMessage = "loading_current_user_information";
     this.isLoading = true;
     this.isSettingContentOpen = {};
     this.settingContents = this.settingsProvider.getSettingContentDetails();
-    if(this.settingContents.length > 0){
+    if (this.settingContents.length > 0) {
       this.toggleSettingContents(this.settingContents[0]);
     }
-    this.userProvider.getCurrentUser().subscribe((currentUser : any)=>{
-      this.currentUser = currentUser;
-      this.currentLanguage = currentUser.currentLanguage;
-      this.loadingMessage = 'loading_settings';
-      this.localInstanceProvider.getLocalInstances().subscribe((instances : any)=>{
-        this.localInstances = instances;
+    this.userProvider.getCurrentUser().subscribe(
+      (currentUser: any) => {
+        this.currentUser = currentUser;
+        this.currentLanguage = currentUser.currentLanguage;
+        this.loadingMessage = "loading_settings";
+        this.localInstanceProvider.getLocalInstances().subscribe(
+          (instances: any) => {
+            this.localInstances = instances;
+            this.isLoading = false;
+          },
+          error => {
+            this.isLoading = false;
+            this.appProvider.setNormalNotification(
+              "Fail to load available local instances"
+            );
+          }
+        );
+      },
+      error => {
+        console.log(error);
         this.isLoading = false;
-      },(error)=>{
-        this.isLoading = false;
-        this.appProvider.setNormalNotification("Fail to load available local instances")
-      });
-    },error=>{
-      console.log(error);
-      this.isLoading = false;
-      this.appProvider.setNormalNotification('Fail to load current user information');
-    });
+        this.appProvider.setNormalNotification(
+          "Fail to load current user information"
+        );
+      }
+    );
   }
 
-
-  updateCurrentLanguage(){
-    try{
+  updateCurrentLanguage() {
+    try {
       let loggedInInInstance = this.currentUser.serverUrl;
-      if(this.currentUser.serverUrl.split("://").length > 1){
+      if (this.currentUser.serverUrl.split("://").length > 1) {
         loggedInInInstance = this.currentUser.serverUrl.split("://")[1];
       }
       this.appTranslationProvider.setAppTranslation(this.currentLanguage);
       this.currentUser.currentLanguage = this.currentLanguage;
-      this.userProvider.setCurrentUser(this.currentUser).subscribe(()=>{});
-      this.localInstanceProvider.setLocalInstanceInstances(this.localInstances,this.currentUser,loggedInInInstance).subscribe(()=>{});
-    }catch (e){
-      this.appProvider.setNormalNotification("Fail to set translation ");
+      this.userProvider.setCurrentUser(this.currentUser).subscribe(() => {});
+      this.localInstanceProvider
+        .setLocalInstanceInstances(
+          this.localInstances,
+          this.currentUser,
+          loggedInInInstance
+        )
+        .subscribe(() => {});
+    } catch (e) {
+      this.appProvider.setNormalNotification("Fail to set translation");
       console.log(JSON.stringify(e));
     }
   }
 
-  toggleSettingContents(content){
-    if(content && content.id){
-      if(this.isSettingContentOpen[content.id]){
+  toggleSettingContents(content) {
+    if (content && content.id) {
+      if (this.isSettingContentOpen[content.id]) {
         this.isSettingContentOpen[content.id] = false;
-      }else{
-        Object.keys(this.isSettingContentOpen).forEach(id=>{
+      } else {
+        Object.keys(this.isSettingContentOpen).forEach(id => {
           this.isSettingContentOpen[id] = false;
         });
         this.isSettingContentOpen[content.id] = true;
       }
     }
   }
-
 }
