04e5cd41924a67009992bb754fb140184642a2c6
Rajab Mkomwa <rajeyw@gmail.com>
2018-05-09 16:16:47 +0300

    feat(widget): Added placeholder for widget visualization

--- src/pages/visualization/modules/index.ts
+++ src/pages/visualization/modules/index.ts
@@ -5,7 +5,8 @@ import { ResourcesModule } from './resources/resources.module';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { FeedbackMessageModule } from './feedback-message/feedback-message.module';
+import { WidgetModule } from './widget/widget.module';
 
 export const modules: any[] = [
-  ChartModule, TableModule, DictionaryModule, ResourcesModule, UsersModule, ReportsModule, FeedbackMessageModule
+  ChartModule, TableModule, DictionaryModule, ResourcesModule, UsersModule, ReportsModule, FeedbackMessageModule, WidgetModule
 ];
