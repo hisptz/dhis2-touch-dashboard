62ce58dbf7d4dbf27f1ed1c2cf06c8ecf0fe28c1
Rajab Mkomwa <rajeyw@gmail.com>
2018-05-10 18:46:11 +0300

    fix(visualization-loader): Fixed UI/UX issues in visualizatio loader

--- src/pages/visualization/components/visualization-card-loader/visualization-card-loader.component.ts
+++ src/pages/visualization/components/visualization-card-loader/visualization-card-loader.component.ts
@@ -7,9 +7,14 @@ import * as _ from "lodash";
 })
 export class VisualizationCardLoaderComponent implements OnInit {
   @Input() visualizationType: string;
+  @Input() height: string;
+  @Input() name: string;
   tableCellCounts: any[];
+  chartBars: any[];
   constructor() {
     this.tableCellCounts = _.range(20);
+    this.chartBars = ['70', '30', '80', '10', '30', '60']
+    this.height = '400px'
   }
 
   ngOnInit() {}
